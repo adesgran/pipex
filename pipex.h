f135269a005623f496abdfa8554cfbc0669a3102
adesgran <adesgran.dev@gmail.com>
2022-05-10 15:18:02 +0200

    Add Struct

--- pipex.h
+++ pipex.h
@@ -6,4 +6,13 @@
 # include <stdlib.h>
 # include <libft.h>
 
+typedef struct	s_cmd
+{
+	char			**cmd;
+	char			*bin_path;
+	int				fd_infile;
+	int				fd_outfile;
+	struct s_cmd	*next;
+}	t_cmd;
+
 #endif
